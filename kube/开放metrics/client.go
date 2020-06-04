0b3c0670cfcdfdac4d098c4ce3f00fac53de5445
legolas-zeng <814382690@qq.com>
2020-06-04 22:23:25 +0800

    自定义metrics

---
+++ kube/开放metrics/client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Unknwon/goconfig"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Cmd struct {
+	ReqType  int
+	FileName string
+}
+
+func main() {
+	cfg, err := goconfig.LoadConfigFile("D:\\GoleGolas\\kube\\开放metrics\\conf.ini")
+	//cfg, err := goconfig.LoadConfigFile("/root/conf.ini")
+	fmt.Println(cfg)
+	if err != nil {
+		log.Println("读取配置文件失败:", err)
+		return
+	}
+
+	url, err := cfg.GetValue("DEFAULT", "url")
+	//fmt.Println(value)
+	if err != nil {
+		log.Println("读取值失败:", err)
+		return
+	}
+	//url := "http://127.0.0.1:8000/handle"
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			log.Println("Post failed:", err)
+			return
+		}
+
+		defer resp.Body.Close()
+
+		content, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Read failed:", err)
+			return
+		}
+
+		log.Println("返回值:", string(content))
+		fmt.Fprint(w, "返回值:", string(content))
+	})
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
