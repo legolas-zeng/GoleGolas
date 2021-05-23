64b7ac97fb1fdd37335e2fdaee84e4a345952a13
legolas-zeng <814382690@qq.com>
2021-05-24 00:34:19 +0800

    字符串中的第一个唯一字符

---
+++ leetcode/初级算法-字符-字符串中的第一个唯一字符/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+//
+//示例：
+//s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+func firstUniqChar(s string) int {
+	hashmap := map[string]int{}
+	for i := 0; i < len(s); i++ {
+		if _, ok := hashmap[string(rune(s[i]))]; ok {
+			fmt.Println("有重复：", string(rune(s[i])))
+			hashmap[string(rune(s[i]))] = 1
+		} else {
+			fmt.Println("存入", string(rune(s[i])), i)
+			hashmap[string(rune(s[i]))] = 0
+		}
+	}
+	fmt.Println(hashmap)
+	for i := 0; i < len(s); i++ {
+		if hashmap[string(rune(s[i]))] == 0 {
+			fmt.Println("第一个不重复", string(rune(s[i])))
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	s := "loveleetcode"
+	fmt.Println(firstUniqChar(s))
+}
