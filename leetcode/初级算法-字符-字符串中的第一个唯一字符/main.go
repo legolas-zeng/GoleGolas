5d263a04ea9843d85fd772c9e1ae430555aa7f55
legolas-zeng <814382690@qq.com>
2021-05-24 15:34:47 +0800

    更新

--- leetcode/初级算法-字符-字符串中的第一个唯一字符/main.go
+++ leetcode/初级算法-字符-字符串中的第一个唯一字符/main.go
@@ -10,6 +10,7 @@ import "fmt"
 //
 //s = "loveleetcode"
 //返回 2
+///////////////////////使用map  hash表////////////////////////////////
 func firstUniqChar(s string) int {
 	hashmap := map[string]int{}
 	for i := 0; i < len(s); i++ {
@@ -22,10 +23,30 @@ func firstUniqChar(s string) int {
 		}
 	}
 	fmt.Println(hashmap)
-	for i := 0; i < len(s); i++ {
-		if hashmap[string(rune(s[i]))] == 0 {
-			fmt.Println("第一个不重复", string(rune(s[i])))
-			return i
+	//for i := 0; i < len(s); i++ {
+	//	if hashmap[string(rune(s[i]))] == 0 {
+	//		fmt.Println("第一个不重复", string(rune(s[i])))
+	//		return i
+	//	}
+	//}
+	for k, v := range s {
+		if hashmap[string(rune(v))] == 0 {
+			return k
+		}
+	}
+	return -1
+}
+
+///////////////////////使用切片 hash表/////////////////////////////////
+func firstUniqChar2(s string) int {
+	hash := [26]int{}
+	for i := range s {
+		hash[s[i]-'a']++
+	}
+	fmt.Println(hash)
+	for k, v := range s {
+		if hash[v-'a'] == 1 {
+			return k
 		}
 	}
 	return -1
