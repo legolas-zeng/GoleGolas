d91ddd2c442e4e387587984344937f6cb42e5f38
legolas-zeng <814382690@qq.com>
2021-05-29 20:51:40 +0800

    合并链表的另外方法和备注

--- leetcode/初级算法-链表-合并两个有序链表/main.go
+++ leetcode/初级算法-链表-合并两个有序链表/main.go
@@ -9,6 +9,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+/////////////////////// 使用递归 //////////////////////////////
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	fmt.Println(l1, l2)
 	if l1 == nil {
@@ -20,12 +21,65 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	var newlist *ListNode
 	if l1.Val < l2.Val {
 		newlist = l1
+		//把小的l1放到newlist中，然后newlist的下一个节点就把l1.Next和l2对比返回回来，以此类推
 		newlist.Next = mergeTwoLists(l1.Next, l2)
 	} else {
 		newlist = l2
 		newlist.Next = mergeTwoLists(l1, l2.Next)
 	}
 	return newlist
+}
+
+//////////////////// 使用递归和替换 ///////////////////////////////
+func mergeTwoLists3(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return l1
+	} else if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+
+	if l2.Val < l1.Val {
+		l1, l2 = l2, l1
+	}
+	l1.Next = mergeTwoLists(l1.Next, l2)
+	return l1
+}
+
+/////////////////////// 普通方法 //////////////////////////////
+func mergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	//var newlist *ListNode
+	head := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	newlist := head
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			newlist.Next = l1
+			l1 = l1.Next
+		} else {
+			newlist.Next = l2
+			l2 = l2.Next
+		}
+		newlist = newlist.Next
+		fmt.Println(newlist, head)
+	}
+	if l1 != nil {
+		newlist.Next = l1
+	}
+	if l2 != nil {
+		newlist.Next = l2
+	}
+	fmt.Println(newlist, head.Next)
+	return head.Next
 }
 
 func main() {
@@ -49,6 +103,6 @@ func main() {
 	ln4.Next = ln5
 
 	//pre := mergeTwoLists(head1,head2)
-	mergeTwoLists(head1, head2)
+	mergeTwoLists2(head1, head2)
 	//fmt.Println(pre)
 }
