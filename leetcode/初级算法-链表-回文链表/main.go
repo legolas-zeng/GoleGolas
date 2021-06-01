73eb17fbbfc8632c512a9f1220a610483633409a
legolas-zeng <814382690@qq.com>
2021-06-01 21:27:42 +0800

    快慢指针法

--- leetcode/初级算法-链表-回文链表/main.go
+++ leetcode/初级算法-链表-回文链表/main.go
@@ -5,6 +5,7 @@ type ListNode struct {
 	Next *ListNode
 }
 
+///////////////////// 使用转换成切片的方式 ///////////////////////////////
 func isPalindrome(head *ListNode) bool {
 	vallist := []int{}
 	for head != nil {
@@ -19,5 +20,45 @@ func isPalindrome(head *ListNode) bool {
 			return false
 		}
 	}
+	return true
+}
+
+/////////////////////  使用快慢指针 /////////////////////////////////////
+
+func isPalindrome2(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+	fast, slow := head, head
+	prev := &ListNode{
+		Val:  nil,
+		Next: nil,
+	}
+	for fast != nil && fast.Next != nil {
+		prev = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	//把slow慢指针断开
+	prev.Next = nil
+
+	// 翻转
+	var head2 *ListNode = nil
+
+	for slow != nil {
+		tmp := slow.Next
+		slow.Next = head2
+		head2 = slow
+		slow = tmp
+	}
+	// 对比
+	for head != nil && head2 != nil {
+		if head.Val != head2.Val {
+			return false
+		}
+		head = head.Next
+		head2 = head.Next
+	}
+
 	return true
 }
